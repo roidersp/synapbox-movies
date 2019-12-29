871ffa0d37b2e4b6cd9b1fee3952df294478b325
Rodrigo Luis Santander <rluis@delarivagroup.com>
2019-12-29 17:08:46 -0600

    configurando apollo client

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import gql from "graphql-tag";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
+
 import * as serviceWorker from "./serviceWorker";
 
 import theme from "./theme";
 import Home from "./Views/Home";
+// import resolvers from './Resolvers';
+
+const apiURL = "https://us1.prisma.sh/john-a-agudelo-e911b8/johnaagudelodb/dev";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: apiURL
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <Home />
-  </ThemeProvider>,
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Home />
+    </ThemeProvider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
